42c03ea6684b1b90b276518f66717cfc251d4533
Brian Freijomil <brianfreijomil@hotmail.com>
2025-02-26 17:44:03 -0300

    add basic home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-portfolio';
+  logoPage = 'B R I A N';
+
 }
