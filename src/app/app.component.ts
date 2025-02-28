ccbdfab2f0c62a26cdecd8dfca78901e3d8610cf
Brian Freijomil <brianfreijomil@hotmail.com>
2025-02-28 19:39:19 -0300

    add challenges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatIconModule],
+  imports: [MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   logoPage = 'B R I A N';
+  linkLinkedin:string = 'https://www.linkedin.com/in/brianfreijomil/'
+  linkGitHub:string = 'https://github.com/brianfreijomil'
+  email:string = 'brianfreijomil@hotmail.com'
+
+  linkPostBengalu:string = "https://www.linkedin.com/feed/update/urn:li:activity:7290031673193164800/?actorCompanyId=101494190";
+  linkTechforbChallenge:string = "https://challengetechforb.netlify.app";
+  linkTechforbChallengeFront:string = "https://github.com/brianfreijomil/desafio-frontend-techforb"
+  linkTechforbChallengeBack:string = "https://github.com/brianfreijomil/desafio-backend-techforb"
+
+  @ViewChild('backApp') backApp!: ElementRef;
+  scrolled = false;
+
+  ngAfterViewInit() {
+    if (this.backApp) {
+      this.backApp.nativeElement.addEventListener('scroll', () => {
+        this.scrolled = this.backApp.nativeElement.scrollTop > 30;
+      });
+    }
+  }
 
 }
